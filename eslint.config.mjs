3b2bec923bd45cc03d5e8f4c5cb7a204f82e5713
jotagadev <henriquesoaresj2@gmail.com>
2025-04-26 18:56:49 -0300

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  ignorePatterns: ["src/generated/prisma/"], 
+});
+
 export default eslintConfig;
